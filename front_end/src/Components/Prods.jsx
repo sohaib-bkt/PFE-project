2ab179b7a2c09ae2476908025af6d99f6700c7a0
Idchaoudi-Youssef <toumiyoussef857@gmail.com>
2024-04-28 01:37:49 +0100

    solve icon prob

--- front_end/src/Components/Prods.jsx
+++ front_end/src/Components/Prods.jsx
@@ -37,8 +37,7 @@ export default function Prods({prod}) {
               <ul>
                 <li>
                   <Link to={`/detail/${prod.id}`} >
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>
-                  </Link>                 
+                  <svg   xmlns="http://www.w3.org/2000/svg"   width={24}   height={24}   viewBox="0 0 24 24"   fill="none"   stroke="currentColor"   strokeWidth={2}   strokeLinecap="round"   strokeLinejoin="round"   className="feather feather-eye" >   <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />   <circle cx={12} cy={12} r={3} /> </svg>                  </Link>                 
                 </li>
                 <li>
                 <a
@@ -47,8 +46,7 @@ export default function Prods({prod}) {
             disabled={loading}
           >
           {loading ? 'Adding to Wishlist...' : (
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>   
-                )}
+            <svg   xmlns="http://www.w3.org/2000/svg"   width={24}   height={24}   viewBox="0 0 24 24"   fill="none"   stroke="currentColor"   strokeWidth={2}   strokeLinecap="round"   strokeLinejoin="round"   className="feather feather-heart" >   <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" /> </svg>                )}
                   </a>
                 </li>
               </ul>
