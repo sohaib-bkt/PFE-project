03d19d25e6a66f42b1b7caa831ebf065950784db
Sohaib Bouktiba <sohaibbouktiba2004@gmail.com>
2024-05-10 04:05:46 +0100

    fix

--- back-end/database/migrations/2024_04_02_222550_add_user_id_to_products_table.php
+++ back-end/database/migrations/2024_04_02_222550_add_user_id_to_products_table.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
 return new class extends Migration
 {
     /**
@@ -12,8 +11,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('products', function (Blueprint $table) {
-        $table->unsignedBigInteger('user_id')->after('id'); // 'id' est une colonne existante après laquelle vous voulez ajouter 'user_id'
-        $table->foreign('user_id')->references('id')->on('users'); // Optionnel: ajouter une contrainte de clé étrangère
+            $table->unsignedBigInteger('user_id')->after('id'); 
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade'); 
         });
     }
 
@@ -23,8 +22,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('user_id');
         });
     }
 };
