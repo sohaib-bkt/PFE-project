7b2bda6f1ea0999164f5c499ac196e2f336566e6
Bourhan <bourhanelahmadi@gmail.com>
2024-05-20 18:31:15 +0100

    fix bug

--- front_end/src/Components/ShopInfofilter.jsx
+++ front_end/src/Components/ShopInfofilter.jsx
@@ -270,35 +270,7 @@ export default function ShopInfofilter() {
   </div>
 
  
-<section className="subscribe-section section-b-space">
-  <div className="container">
-    <div className="row">
-      <div className="col-lg-8 col-md-6">
-        <div className="subscribe-details">
-          <h2 className="mb-3">Subscribe Our News</h2>
-          <h6 className="font-light">
-            Subscribe and receive our newsletters to follow the news about our
-            fresh and fantastic Products.
-          </h6>
-        </div>
-      </div>
-      <div className="col-lg-4 col-md-6 mt-md-0 mt-3">
-        <div className="subsribe-input">
-          <div className="input-group">
-            <input
-              type="text"
-              className="form-control subscribe-input"
-              placeholder="Your Email Address"
-            />
-            <button className="btn btn-solid-default" type="button">
-              Button
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
+
 
 
 </>
